980122264e47711dde871d2cf815c9ce38f812b9
Jaccon <jaccon@gmail.com>
2019-03-23 23:18:51 -0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import Banner from './components/Banner';
-import About from './components/About';
-import Counter from './components/Counter';
-import Project from './components/Projects';
-import LastJobs from './components/LastJobs';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Loader from './components/Loader';
+import Routes from './routes';
 import jQuery from 'jquery';
-
-// import { findDOMNode } from 'react-dom';
 
 // Import Styles
 import './assets/css/animate.min.css';
@@ -23,22 +13,10 @@ import './assets/css/owl.theme.default.min.css';
 // Import third Party JS
 window.$ = jQuery
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Loader/>
-          <Header/>
-          <Banner/>
-          <About/>
-          <Counter/>
-          <Project/>
-          <LastJobs/>
-          <Contact/>
-          <Footer/>
+const App = () => (
+  <div className="App">
+          <Routes />
       </div>
-    );
-  }
-}
+);
 
 export default App;
