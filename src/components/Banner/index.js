62cfa45872e1d7abb783ee813f5ca5624be2ab1b
Jaccon <jaccon@gmail.com>
2019-03-23 21:35:46 -0300

    Fix sidemenu

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import img01 from '../../assets/images/img-app-01.jpg'; 
+
+const divStyle1 = {
+    display: 'none'
+};
+
 const Content = () => (
     <section id="home" className="p-0 bg-img12 center-block">
         <h2 className="d-none">hidden</h2>
@@ -8,20 +14,25 @@ const Content = () => (
                 <div className="row">
                     <div className="col-lg-12 text-left center-col">
                         <div className="personal-box">
-                            <h2 className="font-weight-600">Olá, meu nome é <br/> André Jaccon </h2>
-                            <h3 id="personal"></h3>
+                            <h2 className="font-weight-400 text-uppercase"> André Jaccon </h2>
+                            <h3> 
+                                Self-learning software and computer engineering <br/>
+                                through research & development. 
+                                <br/> Filming the entire process on YouTube.
+                            </h3>
                             <p className="social-icons">
-                                <a href="https://www.linkedin.com/in/jaccon/"> <i class="fab fa-linkedin-in"></i> </a>
-                                <a href="https://api.whatsapp.com/send?phone=+5511955696541&text=Ol%C3%A1%20Andr%C3%A9"> <i class="fab fa-whatsapp"></i> </a>
-                                <a href="https://github.com/jaccon?tab=repositories" target="_blank"> <i class="fab fa-github"></i> </a>
+                                <a href="https://www.youtube.com/user/jaccon" target="_blank"> <i class="fab fa-youtube"></i> </a>
+                                <a href="https://www.linkedin.com/in/jaccon/" target="_blank"> <i class="fab fa-linkedin-in"></i> </a>
+                                <a href="https://api.whatsapp.com/send?phone=+5511955696541&text=Ol%C3%A1%20Andr%C3%A9" target="_blank"> <i class="fab fa-whatsapp"></i> </a>
+                                <a href="https://github.com/jaccon?tab=repositories" target="_blank" > <i class="fab fa-github"></i> </a>
                             </p>
+                            <img src={img01} alt="Jaccon Website" style={divStyle1} />
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-
 );
 
 export default Content;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,11 +13,11 @@ const Header = () => (
 
                 <div className="collapse navbar-collapse" id="wexim">
                     <div className="navbar-nav ml-auto">
-                        <a className="nav-link scroll" href="#home"> André Jaccon</a>
+                        <a className="nav-link scroll" href="#home"> Principal </a>
                         <a className="nav-link scroll" href="#about"> Sobre mim</a>
                         <a className="nav-link scroll" href="#projetos"> Projetos </a>
-                        <a className="nav-link scroll" href="https://github.com/jaccon?tab=repositories"> Github </a>
-                        <a className="nav-link scroll" href="https://blog.jaccon.com.br">Blog</a>
+                        <a className="nav-link" href="https://github.com/jaccon?tab=repositories" target="_blank"> Github </a>
+                        <a className="nav-link" href="http://blog.jaccon.com.br" target="_blank">Blog</a>
                         <a className="nav-link scroll" href="#contato"> Contato </a>
                         <span className="menu-line">
                             <i className="fa fa-angle-down" aria-hidden="true"></i>
@@ -32,6 +32,56 @@ const Header = () => (
                 </a>
             </div>
         </nav>
+
+        <div className="side-menu">
+            <div className="inner-wrapper">
+                <span className="btn-close" id="btn_sideNavClose"><i></i><i></i></span>
+                <nav className="side-nav w-100">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link scroll" href="#home"> André Jaccon </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link scroll" href="#about"> Sobre mim </a>
+                        </li>
+                        
+                        <li className="nav-item">
+                            <a className="nav-link scroll" href="https://blog.jaccon.com.br">Blog</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link scroll" href="https://github.com/jaccon?tab=repositories"> GitHub </a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link scroll" href="#contact"> Podcast </a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link scroll" href="#contact"> Rádio Online </a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link scroll" href="#contact"> Contato </a>
+                        </li>
+
+                       
+
+                    </ul>
+                </nav>
+
+        <div className="side-footer text-white w-100">
+                <ul className="social-icons-simple">
+                    <li><a href="https://www.linkedin.com/in/jaccon/"> <i class="fab fa-linkedin-in"></i> </a></li>
+                    <li><a href="https://api.whatsapp.com/send?phone=+5511955696541&text=Ol%C3%A1%20Andr%C3%A9"> <i class="fab fa-whatsapp"></i> </a></li>
+                    <li><a href="https://github.com/jaccon?tab=repositories" target="_blank"> <i class="fab fa-github"></i> </a></li>
+                </ul>
+                <p className="text-white">&copy; 2019 André Jaccon - Build with ReactJS </p>
+                </div>
+            </div>
+        </div>
+        <a id="close_side_menu" href="javascript:void(0);"></a>
+
     </header>
 )
 
